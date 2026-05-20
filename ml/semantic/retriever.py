f7b2b41a966839ce5d269bbbf72bc4dab58c0c7e
leonardopds441-ux <leonardopds441@gmail.com>
2026-05-20 16:53:21 -0300

    Update retriever.py

--- ml/semantic/retriever.py
+++ ml/semantic/retriever.py
@@ -1,17 +1,14 @@
 from __future__ import annotations
-from pathlib import Path
-from dotenv import load_dotenv
 
 import os
 
 from openai import OpenAI
 from sqlalchemy import create_engine, text
 
-ROOT_DIR = Path(__file__).resolve().parents[2]
-load_dotenv(ROOT_DIR / ".env")
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+print("DEBUG DATABASE_URL =", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 client = OpenAI(api_key=OPENAI_API_KEY)
